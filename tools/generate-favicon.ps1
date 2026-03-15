c10e4567ea51970e5ae39b3abd12ccc6264e79cd
Giorgio Nanni <giorgio.jr.nanni@gmail.com>
2026-03-15 23:56:36 +0000

    Improve favicon visibility from provided icon pack

--- tools/generate-favicon.ps1
+++ tools/generate-favicon.ps1
@@ -1,7 +1,18 @@
 Add-Type -AssemblyName System.Drawing
 
+function New-RoundedRectPath([float]$x, [float]$y, [float]$w, [float]$h, [float]$r) {
+  $path = New-Object System.Drawing.Drawing2D.GraphicsPath
+  $d = $r * 2
+  $path.AddArc($x, $y, $d, $d, 180, 90)
+  $path.AddArc($x + $w - $d, $y, $d, $d, 270, 90)
+  $path.AddArc($x + $w - $d, $y + $h - $d, $d, $d, 0, 90)
+  $path.AddArc($x, $y + $h - $d, $d, $d, 90, 90)
+  $path.CloseFigure()
+  return $path
+}
+
 function Set-GraphicsQuality([System.Drawing.Graphics]$graphics) {
-  $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+  $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
   $graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
   $graphics.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
 }
@@ -64,9 +75,9 @@ if (-not (Test-Path $source)) {
   throw "Source favicon artwork not found: $source"
 }
 
-$cropX = 24
-$cropY = 20
-$cropSize = 123
+$cropX = 20
+$cropY = 18
+$cropSize = 130
 $base = Join-Path $root 'favicon-base-512.png'
 
 $src = [System.Drawing.Bitmap]::FromFile($source)
@@ -74,11 +85,35 @@ $dest = New-Object System.Drawing.Bitmap 512, 512
 $graphics = [System.Drawing.Graphics]::FromImage($dest)
 Set-GraphicsQuality $graphics
 $graphics.Clear([System.Drawing.Color]::Transparent)
+
+$tile = New-RoundedRectPath 18 18 476 476 96
+$bgBrush = New-Object System.Drawing.Drawing2D.LinearGradientBrush `
+  ([System.Drawing.PointF]::new(40, 40)), `
+  ([System.Drawing.PointF]::new(472, 472)), `
+  ([System.Drawing.ColorTranslator]::FromHtml('#0f1116')), `
+  ([System.Drawing.ColorTranslator]::FromHtml('#151922'))
+$graphics.FillPath($bgBrush, $tile)
+
+$outlinePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(30, 255, 255, 255)), 2
+$graphics.DrawPath($outlinePen, $tile)
+
+$glowPath = New-Object System.Drawing.Drawing2D.GraphicsPath
+$glowPath.AddEllipse(60, 250, 280, 190)
+$glowBrush = New-Object System.Drawing.Drawing2D.PathGradientBrush $glowPath
+$glowBrush.CenterColor = [System.Drawing.Color]::FromArgb(130, 255, 42, 0)
+$glowBrush.SurroundColors = @([System.Drawing.Color]::FromArgb(0, 255, 42, 0))
+$graphics.FillEllipse($glowBrush, 60, 250, 280, 190)
+
 $pad = 16
 $destRect = [System.Drawing.Rectangle]::new($pad, $pad, 512 - ($pad * 2), 512 - ($pad * 2))
 $srcRect = [System.Drawing.Rectangle]::new($cropX, $cropY, $cropSize, $cropSize)
 $graphics.DrawImage($src, $destRect, $srcRect, [System.Drawing.GraphicsUnit]::Pixel)
 $dest.Save($base, [System.Drawing.Imaging.ImageFormat]::Png)
+$glowBrush.Dispose()
+$glowPath.Dispose()
+$outlinePen.Dispose()
+$bgBrush.Dispose()
+$tile.Dispose()
 $graphics.Dispose()
 $dest.Dispose()
 $src.Dispose()
